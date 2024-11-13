67e8bdd1ee3be9baba0154398629932b217433fc
Ashe2A <supersonicx001@gmail.com>
2024-11-13 09:55:14 +0100

    JS 2 - anon func needs parenthesis

--- ES6_basic/2-arrow.js
+++ ES6_basic/2-arrow.js
@@ -1,9 +1,9 @@
 export default function getNeighborhoodsList() {
     this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
-  
+
     const self = this;
-    this.addNeighborhood = function(newNeighborhood) {
-      self.sanFranciscoNeighborhoods.push(newNeighborhood);
-      return self.sanFranciscoNeighborhoods;
-    };
-  }
+    this.addNeighborhood = (function(newNeighborhood) {
+        self.sanFranciscoNeighborhoods.push(newNeighborhood);
+        return self.sanFranciscoNeighborhoods;
+    });
+}
