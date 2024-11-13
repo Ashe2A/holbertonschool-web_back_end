1975ae79ad5f9811f5cd4a00069fe05259efbdeb
Ashe2A <supersonicx001@gmail.com>
2024-11-13 15:59:31 +0100

    CSS 10 - var in to let of

--- ES6_basic/4-rest-parameter.js
+++ ES6_basic/4-rest-parameter.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 export default function returnHowManyArguments(...args) {
   let argc = 0;
-  for (const i of args) {
+  for (let i of args) {
     argc += 1;
   }
   return argc;
