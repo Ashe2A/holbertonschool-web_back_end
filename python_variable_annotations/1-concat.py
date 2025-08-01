4c825b634370341f6d3c1f048e91fc38db9c557b
Ashe2A <supersonicx001@gmail.com>
2025-08-01 16:30:17 +0200

    Python - Variable Annotations (1) - Basic annotations - concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Basic annotations - concat"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concat strings method
+
+    Args:
+        str1 (str): first string
+        str2 (str): second string
+
+    Returns:
+        str: Concatened string
+    """
+    return str1 + str2
