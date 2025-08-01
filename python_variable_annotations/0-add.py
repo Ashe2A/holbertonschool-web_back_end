a2ef17aa174fa1bf8fe58b1fad8525d1738ac01c
Ashe2A <supersonicx001@gmail.com>
2025-08-01 16:26:53 +0200

    Python - Variable Annotations (0) - pycode + doc

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python3
 """Basic annotations - add"""
 
+
 def add(a: float, b: float) -> float:
+    """Add floats into float method
+
+    Args:
+        a (float): first float
+        b (float): second float
+
+    Returns:
+        float: Result
+    """
     return a + b
