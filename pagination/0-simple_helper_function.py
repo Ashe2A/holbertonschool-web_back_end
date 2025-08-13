4a55fba329e76c3a1be87a22465851e2976c36d0
Ashe2A <supersonicx001@gmail.com>
2025-08-13 10:42:12 +0200

    Pagination (0) - doc + pycode

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python3
 """Simple helper function"""
 
-def index_range(page, page_size):
+
+def index_range(page: int, page_size: int):
+    """Simple helper
+
+    Args:
+        page (int): Page displayed
+        page_size (int): Number of elements in a page
+
+    Returns:
+        tuple: indexes of the first and last elements displayed
+    """
     return ((page - 1) * page_size, page * page_size)
