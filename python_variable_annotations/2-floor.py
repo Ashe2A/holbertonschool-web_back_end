f9a5d79714db2fd12098fdb3e947cd1b27abfce9
Ashe2A <supersonicx001@gmail.com>
2025-08-13 15:10:39 +0200

    Python - Variable Annotations (2) - Basic annotations - floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - floor
+"""
+
+
+def floor(n: float) -> float:
+    """Floor of a number
+
+    Args:
+        n (float): the number
+
+    Returns:
+        float: The single decimal floored
+            (i.e. pi floors to 3 and 5.9 floors to 5)
+    """
+    return int(n)
