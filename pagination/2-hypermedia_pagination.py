1b2a6b9de9af2444822531ed7bf90a09618e6fc0
Ashe2A <supersonicx001@gmail.com>
2025-08-15 21:37:57 +0200

    Pagination (2) - pycodestyle

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -69,10 +69,14 @@ class Server:
             dict: Dictionary containing the following key-value pairs:
                 "page_size": the length of the returned dataset page
                 "page": the current page number
-                "data": the dataset page (equivalent to return from previous task)
-                "next_page": number of the next page, None if no next page
-                "prev_page": number of the previous page, None if no previous page
-                "total_pages": the total number of pages in the dataset as an integer
+                "data": the dataset page
+                    (equivalent to return from previous task)
+                "next_page": number of the next page,
+                    None if no next page
+                "prev_page": number of the previous page,
+                    None if no previous page
+                "total_pages": the total number of pages
+                    in the dataset as an integer
         """
         dataset = self.dataset()
         total_pages = math.ceil(len(dataset) / page_size)
