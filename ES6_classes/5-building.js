895b9a1570005e843f70ac1a2a3c1ff4f8c3ed78
Ashe2A <supersonicx001@gmail.com>
2025-08-21 13:51:01 +0200

    ES6 Classes (5) - wrong syntax

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -10,9 +10,9 @@ export default class Building {
       && (this.evacuationWarningMessage
         === Building.prototype.evacuationWarningMessage)) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
-
-      this._sqft = sqft;
     }
+
+    this._sqft = sqft;
   }
 
   get sqft() {
