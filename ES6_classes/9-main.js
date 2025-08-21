1d4864788b86e6ec64a8ed349351688b07f13f73
Ashe2A <supersonicx001@gmail.com>
2025-08-21 22:38:32 +0200

    ES6 Classes (9) - Hoisting

---
+++ ES6_classes/9-main.js
@@ -0,0 +1,12 @@
+/* eslint-disable import/extensions */
+/* eslint-disable arrow-parens */
+/* eslint-disable comma-dangle */
+import listOfStudents from './9-hoisting.js';
+
+console.log(listOfStudents);
+
+const listPrinted = listOfStudents.map(
+  student => student.fullStudentDescription
+);
+
+console.log(listPrinted);
