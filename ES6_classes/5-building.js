76491ccd20594d0a120f08999dedb79547f76a7b
Ashe2A <supersonicx001@gmail.com>
2025-08-21 13:50:28 +0200

    ES6 Classes (5) - empty implementation

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,18 +1,25 @@
 // 5. A Building
+/* eslint-disable class-methods-use-this */
 export default class Building {
   constructor(sqft) {
     if (typeof sqft !== 'number') {
       throw new TypeError('SQFT must be a number');
     }
 
-    if (this.constructor !== Building && !this.evacuationWarningMessage) {
+    if ((this.constructor !== Building)
+      && (this.evacuationWarningMessage
+        === Building.prototype.evacuationWarningMessage)) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
-    }
 
-    this._sqft = sqft;
+      this._sqft = sqft;
+    }
   }
 
   get sqft() {
     return this._sqft;
+  }
+
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
