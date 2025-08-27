4c78699ee8414be2640c48ac59724c3a06f33347
Ashe2A <supersonicx001@gmail.com>
2025-08-27 16:55:28 +0200

    ES6 data manipulation (8) - check string directly

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -2,12 +2,9 @@ export default function cleanSet(set, startString) {
   if (!(set instanceof Set)) {
     throw new TypeError('Set must be... a set');
   }
-  if (typeof startString !== 'string') {
-    throw new TypeError('Starting substring must be a string');
-  }
 
   let res = '';
-  if (startString !== '') {
+  if (typeof startString === 'string' && startString !== '') {
     for (const i of set) {
       if ((typeof i === 'string') && i.startsWith(startString)) {
         if (!(res === '')) {
