8dcc57b4178e64db819eb6b898d63393eaa51e89
Ashe2A <supersonicx001@gmail.com>
2025-08-27 10:58:57 +0200

    ES6 data manipulation (4) - Combine

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-undef */
+export default function updateStudentGradeByCity(studentList, city, newGrades) {
+  if (!Array.isArray(studentList) || typeof city !== 'string') {
+    return [];
+  }
+  return studentList.filter((i) => i.location === city).map((i) => {
+    let newGrade = 'N/A';
+    newGrades.forEach((j) => {
+      if (j.studentId === i.id) {
+        newGrade = j.grade;
+      }
+    });
+    return { ...i, newGrade };
+  });
+}
