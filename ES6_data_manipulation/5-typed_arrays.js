f5804286a03864c35c039bb3b7d5de27f10bed4c
Ashe2A <supersonicx001@gmail.com>
2025-08-27 14:57:40 +0200

    ES6 data manipulation (5) - Typed Arrays

---
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-undef */
+export default function createInt8TypedArray(length, position, value) {
+  if (typeof (length) !== 'number') {
+    throw TypeError('Length is a number');
+  }
+  if (typeof (position) !== 'number') {
+    throw TypeError('Position is a number');
+  }
+  if (typeof (value) !== 'number') {
+    throw TypeError('Value is a number');
+  }
+
+  if (position < 0 || position >= length) {
+    throw RangeError('Position outside range');
+  }
+  const buffer = new ArrayBuffer(length);
+  const view = new DataView(buffer);
+  view.setInt8(position, value);
+  return view;
+}
