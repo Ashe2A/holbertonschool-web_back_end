b4dd06c7bf2cc91b11bddf1820fd24743978440d
Ashe2A <supersonicx001@gmail.com>
2025-08-29 22:37:52 +0200

    ES6 Promises (8) - double check test fail

--- ES6_promise/8-try.js
+++ ES6_promise/8-try.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/extensions */
 export default function divideFunction(numerator, denominator) {
-  if (typeof { numerator, denominator } !== 'number') {
+  if (typeof numerator !== 'number' || denominator !== 'number') {
     throw new TypeError('Divide numbers please.');
   }
 
