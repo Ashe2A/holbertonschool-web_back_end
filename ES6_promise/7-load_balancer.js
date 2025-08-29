2052fe13eb2be239b234bff88abc22deaaf3d24d
Ashe2A <supersonicx001@gmail.com>
2025-08-29 21:44:02 +0200

    ES6 Promises (7) - wrong condition

--- ES6_promise/7-load_balancer.js
+++ ES6_promise/7-load_balancer.js
@@ -1,10 +1,10 @@
 /* eslint-disable import/extensions */
 export default function loadBalancer(chinaDownload, USDownload) {
-  if (chinaDownload instanceof Promise) {
+  if (!(chinaDownload instanceof Promise)) {
     throw new Error('You must promise you\'re going to download from China.');
   }
 
-  if (USDownload instanceof Promise) {
+  if (!(USDownload instanceof Promise)) {
     throw new Error('You must promise you\'re going to download from the US.');
   }
 
