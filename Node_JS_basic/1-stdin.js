546311d1a1223db0ecc2e0e65ef0485fbe4a6780
Ashe2A <supersonicx001@gmail.com>
2025-09-01 16:20:48 +0200

    NodeJS Basics (1) - no need for encoding

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,7 +1,5 @@
 console.log('Welcome to Holberton School, what is your name?');
 
-process.stdin.setEncoding('utf8');
-
 process.stdin.on('data', (data) => {
   console.log(`Your name is: ${data}`);
 });
