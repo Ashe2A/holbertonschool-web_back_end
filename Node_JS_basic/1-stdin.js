036f4e59756cdfd54d7a22e309842139e687965e
Ashe2A <supersonicx001@gmail.com>
2025-09-01 16:13:47 +0200

    NodeJS Basics (1) - guess it's ok to loop

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -5,9 +5,6 @@ process.stdin.setEncoding('utf8');
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
   process.stdout.write(`Your name is: ${name}\n`);
-  if (process.stdin.isTTY) {
-    process.exit(0);
-  }
 });
 
 process.stdin.on('end', () => {
