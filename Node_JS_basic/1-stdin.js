f60e0955ba0185c8fab5a543d8aa8af5329c4481
Ashe2A <supersonicx001@gmail.com>
2025-09-01 16:20:24 +0200

    NodeJS Basics (1) - no need to trim string literal does it by itself

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,8 +3,7 @@ console.log('Welcome to Holberton School, what is your name?');
 process.stdin.setEncoding('utf8');
 
 process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  console.log(`Your name is: ${name}`);
+  console.log(`Your name is: ${data}`);
 });
 
 process.stdin.on('end', () => {
