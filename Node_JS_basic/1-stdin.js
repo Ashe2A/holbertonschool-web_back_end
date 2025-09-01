61dd0d8b11493dded8ebd7cbefcce972a8470af7
Ashe2A <supersonicx001@gmail.com>
2025-09-01 15:21:04 +0200

    NodeJS Basics (1) - Ctrl+C + data as data

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,9 +1,16 @@
 console.log('Welcome to Holberton School, what is your name?');
 
-process.stdin.on('data', (input) => {
-  const name = input.toString().trim();
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
   console.log(`Your name is: ${name}`);
+});
 
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+  process.exit(0);
+});
+
+process.stdin.on('exit', () => {
   console.log('This important software is now closing');
   process.exit(0);
 });
