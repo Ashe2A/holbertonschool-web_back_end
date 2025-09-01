547c3e5b5592e090c29ff9b369489ae85d6a22f8
Ashe2A <supersonicx001@gmail.com>
2025-09-01 15:26:09 +0200

    NodeJS Basics (1) - use of stdout, utf8 encoding and check if input is null

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,16 +1,18 @@
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?');
 
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  console.log(`Your name is: ${name}`);
+process.stdin.setEncoding('utf8');
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
 
 process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing');
   process.exit(0);
 });
-
 process.stdin.on('exit', () => {
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing');
   process.exit(0);
 });
