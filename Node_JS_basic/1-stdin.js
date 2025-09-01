41a3faf9ae731406a77c0613574425b0bda0ef62
Ashe2A <supersonicx001@gmail.com>
2025-09-01 14:38:32 +0200

    NodeJS Basics (1) - Using Process stdin

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,8 @@
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.on('data', (input) => {
+  const name = input;
+  console.log(`Your name is: ${name}`);
+});
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+});
