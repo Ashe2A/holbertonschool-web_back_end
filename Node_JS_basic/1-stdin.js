f731b9f962c678184de2843771324cd5d47d65c1
Ashe2A <supersonicx001@gmail.com>
2025-09-01 16:19:46 +0200

    NodeJS Basics (1) - console.log adds a newline by itself

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,12 +1,12 @@
-console.log('Welcome to Holberton School, what is your name?\n');
+console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.setEncoding('utf8');
 
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
-  console.log(`Your name is: ${name}\n`);
+  console.log(`Your name is: ${name}`);
 });
 
 process.stdin.on('end', () => {
-  console.log('This important software is now closing\n');
+  console.log('This important software is now closing');
 });
