1cd0bb3bd883421168ec46a387cee41627a9c16a
Ashe2A <supersonicx001@gmail.com>
2025-09-01 14:46:40 +0200

    NodeJS Basics (1) - exit process with return code 0 when it ends

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,8 +1,11 @@
 console.log('Welcome to Holberton School, what is your name?');
+
 process.stdin.on('data', (input) => {
   const name = input.toString().trim();
   console.log(`Your name is: ${name}`);
 });
+
 process.stdin.on('end', () => {
   console.log('This important software is now closing');
+  process.exit(0);
 });
