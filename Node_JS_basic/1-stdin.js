44697162bc99623c1c25997f66218eaa2dd4ce8d
Ashe2A <supersonicx001@gmail.com>
2025-09-01 15:38:26 +0200

    NodeJS Basics (1) - only end for exiting + closing loop (doesn't seem to work)

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -4,13 +4,10 @@ process.stdin.setEncoding('utf8');
 process.stdin.on('data', (input) => {
   const name = input.toString().trim();
   process.stdout.write(`Your name is: ${name}\n`);
+  process.stdin.end();
 });
 
 process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
-  process.exit(0);
-});
-process.stdin.on('exit', () => {
   process.stdout.write('This important software is now closing\n');
   process.exit(0);
 });
