95c5075e933b123c942a93728337a27037631471
Ashe2A <supersonicx001@gmail.com>
2025-09-01 14:45:08 +0200

    NodeJS Basics (1) - converting into string and trimming

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,6 +1,6 @@
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (input) => {
-  const name = input;
+  const name = input.toString().trim();
   console.log(`Your name is: ${name}`);
 });
 process.stdin.on('end', () => {
