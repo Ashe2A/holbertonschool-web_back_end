08599c4a4c9ddfe6e1c958275fc43afebaaafddf
Ashe2A <supersonicx001@gmail.com>
2025-09-03 10:03:42 +0200

    NodeJS Basics (1) - trying with a different arg than 'data'

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,6 +1,6 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-process.stdin.on('data', (data) => {
-  process.stdout.write(`Your name is: ${data}`);
+process.stdin.on('data', (input) => {
+  process.stdout.write(`Your name is: ${input}`);
   process.exit(0);
 });
 
