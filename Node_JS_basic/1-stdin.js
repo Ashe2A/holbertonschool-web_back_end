b7ba02dcb26c15ff8f1ae23c5f1ef05e9e989555
Ashe2A <supersonicx001@gmail.com>
2025-09-05 13:28:17 +0200

    NodeJS Basics (3) - Reading a file asynchronously with Node JS

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,9 +1,9 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+console.log('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (input) => {
-  process.stdout.write(`Your name is: ${input}`);
+  console.log(`Your name is: ${input}`);
   process.exit(0);
 });
 
 process.on('exit', () => {
-  process.stdout.write('This important software is now closing\n');
+  console.log('This important software is now closing\n');
 });
---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    // Attempt (try) to read the database file asynchronously
+    fs.readFile(path, 'utf8', (e, rawData) => {
+      if (e) {
+        reject(new Error('Cannot load the database'));
+      }
+
+      const rows = rawData.split('\n').filter((i) => i.trim() !== '');
+      // Convert CSV into an array, splitting newlines and trimming empty lines
+      const studentList = rows.slice(1).map((i) => i.split(','));
+      // Map the rows into an actual array
+
+      console.log(`Number of students: ${studentList.length}`);
+
+      const fieldList = {};
+      for (const i of studentList) {
+        const givenName = i[0];
+        const field = i[3];
+        if (!(fieldList[field])) {
+          fieldList[field] = [];
+        }
+        fieldList[field].push(givenName);
+      }
+
+      for (const i of Object.keys(fieldList)) {
+        const fieldStudentList = fieldList[i].join(', ');
+        // join method allows to turn an array into a simple string
+        console.log(`Number of students in ${i}: ${fieldList[i].length}. List: ${fieldStudentList}`);
+      }
+      resolve();
+    });
+  });
+}
+
+module.exports = countStudents;
