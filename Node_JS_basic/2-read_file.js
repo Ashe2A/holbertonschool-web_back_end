4b2f0a47697c3e4aca83d8029584ad4942f882bb
Ashe2A <supersonicx001@gmail.com>
2025-09-05 13:11:36 +0200

    NodeJS Basics (2) - Minor fixes (wasn't working at all)

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,33 +1,31 @@
-import { findSourceMap } from "module";
-
-const fs = require("fs");
+const fs = require('fs');
 
 function countStudents(path) {
   try {
     const rawData = fs.readFileSync(path, 'utf8');
     // Attempt (try) to read the database file synchronously
 
-    const rows = rawData.split('\n').filter(i => i.trim() !== '');
+    const rows = rawData.split('\n').filter((i) => i.trim() !== '');
     // Convert CSV into an array, splitting newlines and trimming empty lines
     const studentList = rows.slice(1).map((i) => i.split(','));
     // Map the rows into an actual array
 
-    process.stdout.write(`Number of students: ${studentList.length}`);
+    process.stdout.write(`Number of students: ${studentList.length}\n`);
 
     const fieldList = {};
-    for (const i in studentList) {
+    for (const i of studentList) {
       const givenName = i[0];
       const field = i[3];
       if (!(fieldList[field])) {
         fieldList[field] = [];
       }
-      fieldList[field].push(firstname);
+      fieldList[field].push(givenName);
     }
 
     for (const i of Object.keys(fieldList)) {
       const fieldStudentList = fieldList[i].join(', ');
       // join method allows to turn an array into a simple string
-      process.stdout.write(`Number of students in ${i}: ${fieldStudentList.length}. List: ${fieldStudentList}`);
+      process.stdout.write(`Number of students in ${i}: ${fieldList[i].length}. List: ${fieldStudentList}\n`);
     }
   } catch (e) {
     throw new Error('Cannot load the database');
